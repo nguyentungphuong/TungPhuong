79cb50e1d04a164e1b93be366b9e6184f14d87b0
nguyentungphuong <125905432+nguyentungphuong@users.noreply.github.com>
2023-02-21 17:13:04 +0700

    Add files via upload

---
+++ NguyenTungPhuong_App.java
@@ -0,0 +1,8 @@
+
+public class NguyenTungPhuong_App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+        System.out.println("NguyenTungPhuong hoc Java");
+		}
+}
